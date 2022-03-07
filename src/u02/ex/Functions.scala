385e6706d2305e7074dc036ad3381a74dbf05b68
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-07 18:33:18 +0100

    Small refactor

--- src/u02/ex/Functions.scala
+++ src/u02/ex/Functions.scala
@@ -3,21 +3,13 @@ package u02.ex
 object Functions extends App:
 
   // a)
-  // val to function literal
   val parity: Int => String = n => if (n % 2 == 0) "even" else "odd"
-
-  // method syntax
   def parity2(x: Int): String = x match
     case n if n % 2 == 0 => "even"
     case _ => "odd"
 
   // b)
   def neg(p: String => Boolean): String => Boolean = !p(_)
-  def negWithGenerics[X](p: X => Boolean): X => Boolean = !p(_)
 
-  val empty: String => Boolean = _ == ""
-  val notEmpty = neg(empty)
-  // test provided by slide
-  println(notEmpty("foo"))
-  println(notEmpty(""))
-  println(notEmpty("foo") && !notEmpty(""))
+  // c
+  def negWithGenerics[X](p: X => Boolean): X => Boolean = !p(_)
