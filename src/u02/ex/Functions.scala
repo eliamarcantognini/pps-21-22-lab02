aba4eef253b86dc828dae82d4ecf739255d459e0
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-07 18:49:44 +0100

    Add test and fun about currying

--- src/u02/ex/Functions.scala
+++ src/u02/ex/Functions.scala
@@ -2,14 +2,20 @@ package u02.ex
 
 object Functions extends App:
 
-  // a)
   val parity: Int => String = n => if (n % 2 == 0) "even" else "odd"
   def parity2(x: Int): String = x match
     case n if n % 2 == 0 => "even"
     case _ => "odd"
 
-  // b)
   def neg(p: String => Boolean): String => Boolean = !p(_)
 
-  // c
   def negWithGenerics[X](p: X => Boolean): X => Boolean = !p(_)
+
+  val valWithCurrying: Double => Double => Double => Boolean =
+    (x: Double) => (y: Double) => (z: Double) => x <= y && y <= z
+    
+  val valWithoutCurrying: (Double, Double, Double) => Boolean = (x, y, z) => x <= y && y <= z
+  
+  def defWithCurrying(x: Double)(y: Double)(z: Double): Boolean = x <= y && y <= z
+  
+  def defWithoutCurrying(x: Double, y: Double, z: Double): Boolean = x <= y && y <= z
