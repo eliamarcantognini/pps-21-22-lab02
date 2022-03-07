21f6cccd9a3e051be403452384d39334406afe30
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-07 18:03:35 +0100

    Add neg and negWithGenerics

--- src/u02/ex/Functions.scala
+++ src/u02/ex/Functions.scala
@@ -2,6 +2,7 @@ package u02.ex
 
 object Functions extends App:
 
+  // a)
   // val to function literal
   val parity: Int => String = _ match
     case n if n % 2 == 0 => "even"
@@ -12,5 +13,17 @@ object Functions extends App:
     case n if n % 2 == 0 => "even"
     case _ => "odd"
 
-  println(parity(10))
-  println(parity2(5))
+  // test parity
+//  println(parity(10))
+//  println(parity2(5))
+
+  // b)
+  val neg: (String => Boolean) => String => Boolean = (p: String => Boolean) => !p(_)
+  def negWithGenerics[X](p: X => Boolean): X => Boolean = !p(_)
+
+  val empty: String => Boolean = _ == ""
+  val notEmpty = neg(empty)
+  // test provided by slide
+  println(notEmpty("foo"))
+  println(notEmpty(""))
+  println(notEmpty("foo") && !notEmpty(""))
---
+++ test/u02/FunctionsTest.scala
@@ -0,0 +1,5 @@
+package u02
+
+class FunctionsTest {
+
+}
