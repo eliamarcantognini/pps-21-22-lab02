05211d4000e9e06e7ee801eb9f003e91abeef06f
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-07 17:51:18 +0100

    Add parity(x)

---
+++ src/u02/ex/part2a.scala
@@ -0,0 +1,16 @@
+package u02.ex
+
+object part2a extends App:
+
+  // val to function literal
+  val parity: Int => String = _ match
+    case n if n % 2 == 0 => "even"
+    case _ => "odd"
+
+  // method syntax
+  def parity2(x: Int): String = x match
+    case n if n % 2 == 0 => "even"
+    case _ => "odd"
+
+  println(parity(10))
+  println(parity2(5))
