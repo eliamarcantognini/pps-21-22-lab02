d7e29a3f1da52ecc32bed49f94fa92f9e8866007
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-07 18:10:18 +0100

    Add parity2 test

--- test/u02/FunctionsTest.scala
+++ test/u02/FunctionsTest.scala
@@ -9,3 +9,8 @@ class FunctionsTest:
   @Test def parityTest(): Unit =
     assertEquals(parity(10), "even")
     assertEquals(parity(5), "odd")
+    
+    
+  @Test def parity2Test(): Unit =
+    assertEquals(parity2(10), "even")
+    assertEquals(parity2(5), "odd")
